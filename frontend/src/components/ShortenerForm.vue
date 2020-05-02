6a49b56dbdfd12ea57f3f7279832982f2307b1c0
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-05-03 03:41:20 +0700

    initial commit

---
+++ frontend/src/components/ShortenerForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="url-shortener">
+    <form action="#" method="post">
+      <div class="form-group">
+        <div class="input-group-prepend">
+          <input type="text" class="form-control" name="">
+        </div>
+        <button class="btn btn-primary btn-block" type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShortenerForm'
+}
+</script>
+
+<style scoped>
+
+</style>
